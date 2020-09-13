6788eede52a3d285158d0574b0b48ac4f5136200
Frankenstone <dampf_acc@online.de>
2020-09-13 07:22:52 +0200

    set int shortcut = 1 to int shortcut = 0

--- src/gui/network_service.cpp
+++ src/gui/network_service.cpp
@@ -132,7 +132,7 @@ int CNetworkServiceSetup::exec(CMenuTarget* parent, const std::string & /*action
 
 int CNetworkServiceSetup::showNetworkServiceSetup()
 {
-	int shortcut = 1;
+	int shortcut = 0;
 
 	CMenuWidget* setup = new CMenuWidget(LOCALE_MAINSETTINGS_NETWORK, NEUTRINO_ICON_SETTINGS, width, MN_WIDGET_ID_NETWORKSETUP_SERVICES);
 
